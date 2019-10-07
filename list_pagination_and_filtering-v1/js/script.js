147430f823baf726e6455236bd0e6a245acb54b7
manniig24 <egatica51@gmail.com>
2019-10-07 01:57:47 -0600

    Finished adding the links to call the showpage function

--- list_pagination_and_filtering-v1/js/script.js
+++ list_pagination_and_filtering-v1/js/script.js
@@ -3,48 +3,70 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
    
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
-
 /*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
+   Two global variable - Student_List will hold an array of students, and the itemsperpage holds 
+   the number of students we want to display on each page.
 ***/
 
-
-
+   let student_list = document.getElementsByClassName('student-item');
+   let itemsPerPage = 10;
 
 /*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
+   The showPage function gets called by the appendPageLinks. The function makes the style 
+   of all the elements in the student_elements hidden. Then using the parameters passed in
+   chooses which elements in the student_list to show.
 ***/
-
-
-
+const showPage = (list, page) => {
+   let start = (page * itemsPerPage) - itemsPerPage;
+   let end = page * itemsPerPage;
+   for (let i = 0; i < list.length; i += 1) {
+      list[i].style.display = "none";
+      if ( i >= start && i < end) {
+         list[i].style.display = '';
+      }
+   }
+}
 
 /*** 
    Create the `appendPageLinks function` to generate, append, and add 
    functionality to the pagination buttons.
+   
 ***/
+const appendPageLinks = (list) => {
+   const totalPages = Math.floor((list.length / 10));
+   const div = document.createElement('div');
+   const page = document.querySelector('.page');
+   const ul = document.createElement('ul');
+   const active = document.querySelectorAll('.active');
+   div.className = 'pagination';
+
+   page.appendChild(div);
+   div.appendChild(ul);
+
+   for ( let i = 0; i <= totalPages; i++) {
+       const li = document.createElement('li');
+       const a = document.createElement('a');
+       a.textContent = i + 1;
+       li.appendChild(a);
+       div.appendChild(li);
+       active[i].className  = '';
+       a.addEventListener('click', (e) => {
+         showPage(student_list, i + 1);
+        
+        e.target.className = 'active';
+       });
+      
+    }
+   /*
+   6. Loop over pagination links to remove active class from all links
+   7. Add the active class to the link that was just clicked. You can identify that
+   clicked link using event.target
+   */
+   }
+   showPage(student_list, 1);
+   appendPageLinks(student_list);
 
 
 
 
 
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
